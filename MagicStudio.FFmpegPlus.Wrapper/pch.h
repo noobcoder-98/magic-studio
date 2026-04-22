0de65494eae7c1210b10602f700a5268a8a54ad1
anoobcoder98 <anoobcoder98@gmail.com>
2026-04-22 22:43:31 +0700

    feat: add SDL2 lib

--- MagicStudio.FFmpegPlus.Wrapper/pch.h
+++ MagicStudio.FFmpegPlus.Wrapper/pch.h
@@ -3,7 +3,7 @@
 // Pull in native code without managed interference.
 #pragma managed(push, off)
 #include "../MagicStudio.FFmpegPlus.Native/pch.h"
-#include "../MagicStudio.FFmpegPlus.Native/MediaDecoder.h"
+#include "../MagicStudio.FFmpegPlus.Native/MagicPlayer/MediaDecoder.h"
 #pragma managed(pop)
 
 #include <msclr/marshal_cppstd.h>
